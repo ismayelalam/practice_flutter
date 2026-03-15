393e667d83e55c9e1df2eed087544cf437d17bfd
Ismayel Alam <ismayelalam0@gmail.com>
2026-03-15 15:42:34 +0600

    add character basic data model

---
+++ test_application_01/lib/models/character.dart
@@ -0,0 +1,12 @@
+class Character {
+  final String name;
+  final String slogan;
+  final String id;
+  bool _isFav = false;
+
+  Character({required this.name, required this.slogan, required this.id});
+
+  void toggleIsFav() {
+    _isFav = !_isFav;
+  }
+}
