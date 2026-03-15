0cc20eeee08c81583a96b4693fba3f3278a12338
Ismayel Alam <ismayelalam0@gmail.com>
2026-03-15 16:16:27 +0600

    add vocation enum

--- test_application_01/lib/models/character.dart
+++ test_application_01/lib/models/character.dart
@@ -1,12 +1,19 @@
 import 'package:test_application_01/models/stats.dart';
+import 'package:test_application_01/models/vocation.dart';
 
 class Character with Stats {
   final String name;
   final String slogan;
   final String id;
+  final Vocation vocation;
   bool _isFav = false;
 
-  Character({required this.name, required this.slogan, required this.id});
+  Character({
+    required this.id,
+    required this.name,
+    required this.slogan,
+    required this.vocation,
+  });
 
   bool get isFav => _isFav;
 
